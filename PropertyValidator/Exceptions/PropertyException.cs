bdd7c4274b79d317bf1bf2b3f1e49b81e7229d12
mark <dev.kram5@gmail.com>
2021-04-10 21:26:14 +0800

    Include info on custom exception

--- PropertyValidator/Exceptions/PropertyException.cs
+++ PropertyValidator/Exceptions/PropertyException.cs
@@ -5,9 +5,11 @@ namespace PropertyValidator.Exceptions
 {
     public class PropertyException : Exception
     {
+        public ValidationResultArgs ValidationResultArgs { get; }
+
         public PropertyException(ValidationResultArgs validationResultArgs) : base(validationResultArgs.FirstError)
         {
-
+            ValidationResultArgs = validationResultArgs;
         }
     }
 }
