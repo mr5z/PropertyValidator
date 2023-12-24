cb81f554647b41309daeda2dfa3003e7f62dbba5
mark <dev.kram5@gmail.com>
2023-12-24 15:03:25 +0800

    Add more unit tests and code refactor

--- src/Exceptions/PropertyException.cs
+++ src/Exceptions/PropertyException.cs
@@ -3,6 +3,10 @@ using System;
 
 namespace PropertyValidator.Exceptions;
 
+/// <summary>
+/// Exception thrown when some property violated a certain <see cref="IValidationRule"/>.
+/// </summary>
+/// <param name="validationResultArgs">Result obtained after validation.</param>
 public class PropertyException(ValidationResultArgs validationResultArgs) : Exception(validationResultArgs.FirstError)
 {
     public ValidationResultArgs ValidationResultArgs { get; } = validationResultArgs;
